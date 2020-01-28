b4eb8597333c3c34c3194985215eaffa3ee0768b
Ekaterina <pirit544@gmail.com>
2020-01-28 16:14:04 +0500

    added secret_key.txt

--- notes/settings.py
+++ notes/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2l#$3)75r3_bu7ga@1qvmd8+de81f*dv7(emz@r_chj3ng2+$='
+with open('./secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
